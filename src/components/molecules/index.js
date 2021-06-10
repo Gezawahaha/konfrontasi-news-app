cee9cf1599f53fe078db045288c1caff977791a3
Gezawahaha <60805115+Gezawahaha@users.noreply.github.com>
2021-06-10 10:56:33 +0700

    upploud

--- src/components/molecules/index.js
+++
@@ -1,8 +0,0 @@
-import Header from './Header';
-import Footer from './Footer';
-import Main from './Main';
-import BlogMain from './BlogMain';
-import Contacts from './Contacts';
-import BlogPost from './BlogPost';
-
-export {Header, Footer, Main, BlogMain , Contacts, BlogPost};
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import Home from './Home';
-import Blog from './Blog';
-
-export {Home, Blog}
